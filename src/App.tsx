11ff76d4a1172623da10a416e8c9bd0d9e88b6dd
Sachith-SL <itsmesachith@gmail.com>
2025-08-14 21:39:52 +0530

    add basic structure

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+function App() {
+
+
+  return (
+    <>
+      <h1>Book Me Application</h1>
+      <div className="card">
+        <p>
+          this is online booking system
+        </p>
+      </div>
+      <div>
+        <button>Test Button</button>
+      </div>
+    </>
+  )
+}
+
+export default App
